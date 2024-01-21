99a07ee24bb0b2f4911c186c203de0f14d284468
Lalita Saetan <saetan_l@silpakorn.edu>
2024-01-21 12:15:18 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/hello_list_cpsu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hello_list_cpsu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,230 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  List<Map<String, dynamic>> data = [
+    {
+      'title': 'Favorite',
+      'icon': Icons.favorite,
+    },
+    {
+      'title': 'Home',
+      'icon': Icons.home,
+    },
+    {
+      'title': 'Person',
+      'icon': Icons.person,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+    {
+      'title': 'Key',
+      'icon': Icons.key,
+    },
+  ];
+
+  // ภาษา Dart จะทำการ infer type ให้เรา (type inference)
+  var numbers = [1, 2, 3];
+
+  @override
+  Widget build(BuildContext context) {
+    // var title = data[0]['title'];
+    // var icon = data[0]['icon'];
+
+    var firstItem = data[0];
+    var title = firstItem['title'];
+    var value = firstItem['value'];
+
+    // Random r = Random();
+    // var index = r.nextInt(data.length);
+    // var title = data[index]['title'];
+    // var icon = data[index]['icon'];
+
+    return Scaffold(
+      body: Container(
+        color: Colors.pink,
+        child: Center(
+          child: ListView.builder(
+            // Named constructor
+            itemCount: data.length,
+            itemBuilder: buildItem,
+          ),
+          // child: Column(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     Icon(icon, size: 50, color: Colors.white),
+          //     SizedBox(height: 10.0,),
+          //     Text(title, style: TextStyle(color: Colors.white)),
+          //   ],
+          // ),
+        ),
+      ),
+    );
+  }
+
+  Widget? buildItem(BuildContext context, int index) {
+    var title = data[index]['title'];
+    var icon = data[index]['icon'];
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        border: Border.all(width: 4.0, color: Colors.white),
+        borderRadius: BorderRadius.circular(4.0),
+        // boxShadow: [
+        //   BoxShadow(
+        //     offset: Offset(0, 0),
+        //     color: Colors.grey,
+        //     spreadRadius: 4.0,
+        //     blurRadius: 4.0,
+        //   ),
+        // ]
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 2.0, color: Colors.red),
+              //borderRadius: BorderRadius.circular(64.0),
+              shape: BoxShape.circle,
+            ),
+            child: Icon(icon, color: Colors.blue),
+          ),
+          SizedBox(width: 8.0),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Roasted Chickpea'),
+                Text('\$12.99'),
+              ],
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                width: 24.0,
+                height: 24.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Text('+'),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text('222222222'),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: 24.0,
+                height: 24.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Text('-'),
+              ),
+            ],
+          ),
+          // Responsive, Adaptive
+        ],
+      ),
+    );
+  }
+}
